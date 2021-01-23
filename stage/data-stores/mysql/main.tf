31ea5e99c0899feed73c4654d155213953bdc331
Josh <jduston9652@gmail.com>
2021-01-23 09:45:24 -0800

    Inital commit of Project

---
+++ stage/data-stores/mysql/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "josh-s3-terraform-state"
+    key            = "stage/data-stores/mysql/terraform.tfstate"
+    region         = "us-east-2"
+
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt        = true
+  }
+}
+
+ provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_db_instance" "example" {
+  identifier_prefix   = "terraform-up-and-running"
+  engine              = "mysql"
+  allocated_storage   = 10
+  instance_class      = "db.t2.micro"
+  name                = "example_database"
+  username            = "admin"
+  skip_final_snapshot = true
+
+  # How should we set the password?
+  password            = var.db_password
+}
