1e12e71bb3cc6bf158d8fcc577771d54559dabcb
Josh <jduston9652@gmail.com>
2021-01-23 14:05:59 -0800

    update to stage

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -4,4 +4,8 @@ provider "aws" {
 
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
+  
+    cluster_name           = "webservers-stage"
+    db_remote_state_bucket = "josh-state-files"
+    db_remote_state_key    = "stage/services/webserver-cluster/terraform.tfstate"
 }
