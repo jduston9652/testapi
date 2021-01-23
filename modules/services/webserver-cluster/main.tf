d2634cb651a82d0b3102f9107461f8184a3cdd76
Josh <jduston9652@gmail.com>
2021-01-23 13:41:27 -0800

    update to moduels

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -42,7 +42,6 @@ resource "aws_launch_configuration" "example" {
   image_id        = "ami-0a91cd140a1fc148a"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.instance.id]
-  user_data       = data.template_file.user_data.rendered
               
         # Required when using a launch configuration with an auto scaling group.
         # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
@@ -84,16 +83,6 @@ data "terraform_remote_state" "db" {
     bucket = "josh-state-files"
     key    = "stage/data-stores/mysql/terraform.tfstate"
     region = "us-east-2"
-  }
-}
-
-data "template_file" "user_data" {
-  template = file("user-data.sh")
-
-  vars = {
-    server_port = var.server_port
-    db_address  = data.terraform_remote_state.db.outputs.address
-    db_port     = data.terraform_remote_state.db.outputs.port
   }
 }
 
